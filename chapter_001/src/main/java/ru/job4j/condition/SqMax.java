f669177045b1df85c11e8ea547ead02c38ef9757
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-04 00:04:50 +0300

    add sqMax task

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+public class SqMax {
+
+    public static int max(int first, int second, int third, int forth) {
+        //        int result = first;
+        //        if (first > second) {
+        //
+        //            if (first > third) {
+        //
+        //                if (first > forth) {
+        //                    result = first;
+        //                }
+        //            }
+        //        } else if (second > third) {
+        //            if (second > forth) {
+        //                result = second;
+        //            }
+        //        } else if (third > forth) {
+        //            result = third;
+        //        }
+        //        return result;
+        //    }
+        int max1 = first >= second ? first : second;
+        int max2 = third >= forth ? third : forth;
+        return max1 >= max2 ? max1 : max2;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SqMaxTest {
+
+    @Test
+    public void whenMaxFirst() {
+
+        int result = SqMax.max(4, 3, 2, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMaxSecond() {
+
+        int result = SqMax.max(3, 4, 2, 1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenMaxThird() {
+
+        int result = SqMax.max(4, 3, 7, 2);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMaxFourth() {
+
+        int result = SqMax.max(4, 3, 2, 7);
+        assertThat(result, is(7));
+    }
+}
