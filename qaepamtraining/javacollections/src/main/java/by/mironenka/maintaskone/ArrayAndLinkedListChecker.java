12f42435f7763a265d3374bccb8288010f836eae
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-03 16:24:04 +0300

    add some changes to README.md

---
+++ qaepamtraining/javacollections/src/main/java/by/mironenka/maintaskone/ArrayAndLinkedListChecker.java
@@ -0,0 +1,22 @@
+package by.mironenka.maintaskone.arraylistversion;
+
+import by.mironenka.maintaskone.model.Human;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayAndLinkedListChecker {
+
+    public static void checkListOfHumans(List<Human> list) {
+
+        while (list.size() > 1) {
+            Iterator iterator = list.iterator();
+            for (int i = 0; iterator.hasNext(); i++, iterator.next()) {
+                if (i % 2 == 1) {
+                    i = 0;
+                    iterator.remove();
+                }
+            }
+        }
+    }
+}
