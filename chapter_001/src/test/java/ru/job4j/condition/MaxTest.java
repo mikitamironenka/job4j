b0c4e543aa42383de386db5364de7afaff8151ec
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-03 20:08:18 +0300

    add max task

---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+        Max max = new Max();
+        int result = max.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax2To1Then1() {
+        Max max = new Max();
+        int result = max.max(2, 1);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenNumbersEqual() {
+        Max max = new Max();
+        int result = max.max(2, 2);
+        assertThat(result, is(2));
+    }
+
+}
