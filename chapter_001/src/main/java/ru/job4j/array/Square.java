5af253747c8afe43cc6a9b08a63fd9e25fbad12f
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-04 20:05:51 +0300

    add square numbers in array task

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * Square numbers in array
+ */
+public class Square {
+
+    /**
+     * Square numbers
+     * @param bound
+     * @return array with square numbers
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        // заполнить массив через цикл элементами от 1 до bound возведенными в квадрат
+        for(int i = 0; i <= rst.length - 1; i++) {
+
+            rst[i] = (int) Math.pow((i + 1), 2);
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for array task.
+ *
+ * @author Mikita Mironenka (m.mironenka@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.array;
