44aa66d93fa847d37b6c8ee823b829db364bc3a5
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-05 20:14:29 +0300

    add change method mono in Check class

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,12 +2,10 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
-        boolean booleanInData = data[0];
+        boolean result = true;
+        boolean first = data[0];
         for (int i = 0; i < data.length; i++) {
-            if(data[i] == booleanInData) {
-                result = true;
-            } else {
+            if(data[i] != first) {
                 result = false;
                 break;
             }
