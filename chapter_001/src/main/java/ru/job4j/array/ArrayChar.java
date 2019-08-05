3a1fb2677874ca6113e64617fa511b1128f383f1
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-05 20:21:58 +0300

    add arrayChar task

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean startsWith(String word, String prefix) {
+        boolean result = true;
+        char[] pref = prefix.toCharArray();
+        char[] wrd = word.toCharArray();
+        // проверить. что массив data имеет первые элементы одинаковые с value
+        for(int i = 0; i < pref.length; i++) {
+            if(wrd[i] != pref[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
