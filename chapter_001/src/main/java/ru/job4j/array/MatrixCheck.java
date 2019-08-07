288de7010ed7acb9df2b46232094e499a3be3480
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-07 21:33:23 +0300

    changed mono() method in MatrixCheck

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,17 +5,16 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
 
-        if (data.length == 2) {
-            if (data[0][0] != data[1][1] || data[0][1] != data[1][0]) {
-                result = false;
-            }
-        } else {
-            for (int i = 0; i < data.length; i++) {
-                if (data[1][1] != data[i][i] || data[1][1] != data[data.length - 1 - i][i]) {
+        for (int i = 0; i < data.length; i++) {
+                if (data[i][i] != data[0][0]) {
                     result = false;
+                    break;
+                }
+                if(data[i][data.length - 1 - i] != data[data.length - 1 - i][i]) {
+                    result = false;
+                    break;
                 }
             }
-        }
         return result;
     }
 }
