cec76ec257c27cc0f49358c3a79d96a1559cfdf7
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-08 21:28:14 +0300

    added logic class

--- chapter_001/src/main/java/ru/job4j/Main.java
+++ chapter_001/src/main/java/ru/job4j/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, job4j");
+
+        int[][] data = { {1, 2 ,3},
+                {4, 5, 6}};
+        System.out.println(data[1][2]);
     }
 }
