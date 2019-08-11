845ed64bcaebe1d2fcbf728bc79887065b9cd17f
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-11 15:05:23 +0300

    2. Рефакторинг - Площадь треугольника.[#165665]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,6 +2,16 @@ package ru.job4j.condition;
 
 public class Triangle {
 
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
     /**
      * Метод вычисления полупериметра по длинам сторон.
      *
@@ -43,11 +53,11 @@ public class Triangle {
      *
      * @return Вернуть площадь, если треугольник существует или -1.
      */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area(Point first, Point second, Point third) {
         double rsl = -1;
-        double a = new Point(x1, y1).distance(new Point(x2, y2));
-        double b = new Point(x2, y2).distance(new Point(x3, y3));
-        double c = new Point(x1, y1).distance(new Point(x3, y3));
+        double a = first.distance(second);
+        double b = second.distance(third);
+        double c = first.distance(third);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             // написать формулу для расчета площади треугольника.
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -10,9 +10,12 @@ public class TriangleTest {
     @Test
     public void whenAreaSetThreePointsThenTriangleArea() {
         // Создаем объект треугольник.
-        Triangle triangle = new Triangle();
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 2);
+        Point third = new Point(2, 0);
+        Triangle triangle = new Triangle(first, second, third);
         // Вычисляем площадь.
-        double result = triangle.area(0, 0, 0, 2, 2, 0);
+        double result = triangle.area(first, second, third);
         // Задаем ожидаемый результат.
         double expected = 2D;
         //Проверяем результат и ожидаемое значение.
