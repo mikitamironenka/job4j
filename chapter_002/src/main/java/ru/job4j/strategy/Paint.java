ab71f10f31d5dadcd82e050a98e099dfffb007ec
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-18 16:36:32 +0300

    4. [#165652] refactor test class

--- chapter_002/src/main/java/ru/job4j/strategy/Paint.java
+++ chapter_002/src/main/java/ru/job4j/strategy/Paint.java
@@ -5,10 +5,4 @@ public class Paint {
     public void draw(Shape shape) {
         System.out.println(shape.draw());
     }
-
-    public static void main(String[] args) {
-
-        new Paint().draw(new Triangle());
-        new Paint().draw(new Square());
-    }
 }
