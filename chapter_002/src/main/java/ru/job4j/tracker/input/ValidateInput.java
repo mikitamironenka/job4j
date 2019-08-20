4def31839d6d7935397ca5851b507f9b036091eb
Mikita Mironenka <m.mironenka@gmail.com>
2019-08-20 16:22:38 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#165655]

--- chapter_002/src/main/java/ru/job4j/tracker/input/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/input/ValidateInput.java
@@ -4,19 +4,31 @@ import ru.job4j.tracker.exception.MenuOutException;
 
 import java.util.List;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+
+    private final Input input;
+
+    public ValidateInput(Input input) {
+        this.input = input;
+    }
+
+
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
 
     public int ask(String question, List<Integer> range) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch(MenuOutException moe) {
                 System.out.println("Please, select key from menu");
             } catch(NumberFormatException nfe) {
-                System.out.println("Please, enter validate data again");
+                System.out.println("Please, enter validate data again.");
             }
         } while(invalid);
         return value;
---
+++ chapter_002/src/test/java/ru/job4j/tracker/input/ValidateInputTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker.input;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ValidateInputTest {
+
+    private final ByteArrayOutputStream mem = new ByteArrayOutputStream();
+    private final PrintStream out = System.out;
+
+    @Before
+    public void loadMem() {
+        System.setOut(new PrintStream(this.mem));
+    }
+
+    @After
+    public void loadSys() {
+        System.setOut(this.out);
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        List<Integer> list =  new ArrayList<>();
+        list.add(1);
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"invalid", "1"})
+        );
+        input.ask("Enter", list);
+        assertThat(
+                this.mem.toString(),
+                is(
+                        String.format("Please, enter validate data again.%n")
+                )
+        );
+    }
+
+}
