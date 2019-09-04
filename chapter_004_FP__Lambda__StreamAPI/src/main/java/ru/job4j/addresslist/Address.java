2fef4636efa691057f8eff5ad2f86cbde2ef4b40
Mikita Mironenka <m.mironenka@gmail.com>
2019-09-04 22:23:27 +0300

    3. Уникальность элементов и сортировка.[#165509]

--- chapter_004_FP__Lambda__StreamAPI/src/main/java/ru/job4j/addresslist/Address.java
+++ chapter_004_FP__Lambda__StreamAPI/src/main/java/ru/job4j/addresslist/Address.java
@@ -65,4 +65,14 @@ public class Address {
     public int hashCode() {
         return Objects.hash(city, street, home, apartment);
     }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city
+                +", street='" + street
+                +", home=" + home
+                +", apartment=" + apartment +
+                '}';
+    }
 }
--- chapter_004_FP__Lambda__StreamAPI/src/test/java/ru/job4j/addresslist/ProfilesTest.java
+++ chapter_004_FP__Lambda__StreamAPI/src/test/java/ru/job4j/addresslist/ProfilesTest.java
@@ -24,4 +24,23 @@ public class ProfilesTest {
         List<Address> checked = List.of(ad1, ad2, ad3);
         assertThat(result, is(checked));
     }
+
+    @Test
+    public void whenCollectUniqueThenReturnListOfUniqueAddresses() {
+        Profiles profiles = new Profiles();
+        Address ad1 = new Address("NY", "st", 55, 11);
+        Address ad2 = new Address("LA", "br", 22, 205);
+        Address ad3 = new Address("SF", "zz", 11, 325);
+        Address ad4 = new Address("SF", "zz", 11, 325);
+        Address ad5 = new Address("AA", "bb", 05, 777);
+        Profile pr1 = new Profile(ad1);
+        Profile pr2 = new Profile(ad2);
+        Profile pr3 = new Profile(ad3);
+        Profile pr4 = new Profile(ad4);
+        Profile pr5 = new Profile(ad5);
+        List<Profile> profilesList = List.of(pr1, pr2, pr3, pr4, pr5);
+        List<Address> result = profiles.collectUniqueAddresses(profilesList);
+        List<Address> checked = List.of(ad5, ad2, ad1, ad3);
+        assertThat(result, is(checked));
+    }
 }
