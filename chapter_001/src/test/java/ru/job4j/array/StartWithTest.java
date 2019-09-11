ba5d65ca4eec3f668fc30348a38a67e41b8f57bf
Mikita Mironenka <m.mironenka@gmail.com>
2019-09-11 20:12:46 +0300

    6.4. Слово начинается с ...[#165555]

---
+++ chapter_001/src/test/java/ru/job4j/array/StartWithTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StartWithTest {
+
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'e'};
+        boolean result = StartWith.startsWith(word, pref);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'i'};
+        boolean result =StartWith.startsWith(word, pref);
+        assertThat(result, is(false));
+    }
+
+}
