38fc7fa47a5aca5fb7ff79120ebb2a4be68d0da3
Mikita Mironenka <m.mironenka@gmail.com>
2019-09-15 11:05:49 +0300

    5.1.1. Итератор для двухмерного массива int[][]

---
+++ chapter_005_ Collections_Pro/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,59 @@
+package ru.job4j.iterator;
+
+//Необходимо создать итератор для двухмерного массива.
+//int[][] value = {
+//   {1, 2}
+//   {3, 4}
+//};
+//метод next = должен вернуть последовательно 1, 2, 3, 4.
+//Старайтесь написать универсальное решение, чтобы оно не было жестко ориентировано на тестовый пример.
+//И хотя в примере указана квадратная матрица, программа должна корректно обрабатывать и jagged array тоже.
+//Пример jagged array - {{1},{2, 3, 4, 5,},{6, 7}, {8, 9, 10, 11, 12, 13, 14}}
+//Не используйте в данном задании коллекции из JDK, вспомогательные массивы и т.д.
+//Постарайтесь реализовать последовательным проходом по массиву.
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIterator implements Iterator<Integer> {
+
+    private final int[][] value;
+
+    public MatrixIterator(int[][] value) {
+        this.value = value;
+    }
+
+    private int row = 0;
+    private int column = 0;
+
+    @Override
+    public boolean hasNext() {
+        return row < value.length && column < value[row].length;
+    }
+
+    private void resetIndexes()
+    {
+        if (column == value[row].length)
+        {
+            column = 0;
+            row++;
+        }
+    }
+
+    @Override
+    public Integer next() {
+        while (row < value.length) {
+            while (column < value[row].length) {
+                int irr = value[row][column];
+                column++;
+                resetIndexes();
+                return irr;
+            }
+            row++;
+        }
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return null;
+    }
+}
---
+++ chapter_005_ Collections_Pro/src/test/java/ru/job4j/iterator/JaggedArrayIterator.java
@@ -0,0 +1,49 @@
+package ru.job4j.iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import java.util.Iterator;
+
+public class JaggedArrayIterator {
+
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp(){
+        it = new MatrixIterator(new int[][]{{1}, {3, 4}, {7}});
+    }
+
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation () {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder () {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void hasNextNextSequentialInvocation () {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(false));
+    }
+}
