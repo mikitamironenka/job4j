714577cbb1c6408c5664c81b283681deb0fe31f9
Mikita Mironenka <m.mironenka@gmail.com>
2019-09-20 22:05:43 +0300

    fixed converter task

--- chapter_005_ Collections_Pro/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_005_ Collections_Pro/src/main/java/ru/job4j/iterator/Converter.java
@@ -1,40 +1,19 @@
 package ru.job4j.iterator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.List;
 
 public class Converter {
 
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
-        return new Iterator<Integer>() {
-
-            private Iterator<Integer> currentIterator = it != null && it.hasNext() ? it.next() : null;
-
-            @Override
-            public boolean hasNext() {
-                selectCurrentIterator();
-                return currentIterator != null;
+        List<Integer> integers = new ArrayList<>();
+        while (it.hasNext()) {
+            Iterator<Integer> iterator = it.next();
+            while (iterator.hasNext()) {
+                integers.add(iterator.next());
             }
-
-            @Override
-            public Integer next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                return this.currentIterator.next();
-            }
-
-            private void selectCurrentIterator() {
-                if (this.currentIterator != null && !this.currentIterator.hasNext()) {
-                    this.currentIterator = it.hasNext() ? it.next() : null;
-                    this.selectCurrentIterator();
-                }
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
+        }
+        return integers.iterator();
     }
 }
