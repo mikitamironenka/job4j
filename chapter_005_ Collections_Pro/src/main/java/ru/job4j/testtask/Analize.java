77e4101008778efb2dbda65e7d262b77bdfce813
Mikita Mironenka <m.mironenka@gmail.com>
2019-10-01 23:04:22 +0300

    Статистику по коллекции.[#180998]

---
+++ chapter_005_ Collections_Pro/src/main/java/ru/job4j/testtask/Analize.java
@@ -0,0 +1,114 @@
+package ru.job4j.testtask;
+
+//Это задание сводиться к определению разницы между начальным состояние массива и измененным.
+//Например. Дан массива чисел.
+//Начальное состояние 1 10 13 4 5
+//Конечное состояние 1 13 4
+//Разница будет будет массив 10 5.
+//Это элементарный пример. Ваша задача более сложная.
+//метод должен возвращать статистику об изменении коллекции.
+//List<User> previous - начальные данные,
+//List<User> current - измененные данные.
+//Нужно понять:
+//Сколько добавлено новых пользователей.
+//Сколько изменено пользователей. Изменённым считается объект в котором изменилось имя. а id осталось прежним.
+//Сколько удалено пользователей.
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Analize {
+
+    public Info diff(List<User> previous, List<User> current) {
+        Info result = new Info();
+        int users = newUsers(previous, current);
+        if (users < 0) {
+            result.setDeleted(users);
+        } else if (users > 0) {
+            result.setAdded(users);
+        }
+        result.setChanged(User.edited);
+        return result;
+    }
+
+    /**
+     * Method to return count of added or deleted users.
+     * @param previous list of users before changing (adding, deleting, editing user's names)
+     * @param current list of users after changing
+     * @return int - count of of added or deleted users.
+     */
+    public int newUsers(List<User> previous, List<User> current) {
+        int result = 0;
+        Set<Integer> prevKeys = new HashSet<>();
+        Set<Integer> currKeys = new HashSet<>();
+        for (User user : previous) {
+            prevKeys.add(user.id);
+        }
+        for (User user : current) {
+            currKeys.add(user.id);
+        }
+        result = currKeys.size() - prevKeys.size();
+        return result;
+    }
+
+    public static class User {
+        int id;
+        String name;
+        public static int edited = 0;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+            edited++;
+        }
+    }
+
+    public static class Info {
+
+        int added;
+        int changed;
+        int deleted;
+
+        public Info() {}
+
+        public int getAdded() {
+            return added;
+        }
+
+        public void setAdded(int added) {
+            this.added = added;
+        }
+
+        public int getChanged() {
+            return changed;
+        }
+
+        public void setChanged(int changed) {
+            this.changed = changed;
+        }
+
+        public int getDeleted() {
+            return deleted;
+        }
+
+        public void setDeleted(int deleted) {
+            this.deleted = deleted;
+        }
+
+        @Override
+        public String toString() {
+            return "Info{"
+                    + "added=" + added
+                    + ", changed=" + changed
+                    + ", deleted=" + deleted
+                    + '}';
+        }
+    }
+
+
+}
---
+++ chapter_005_ Collections_Pro/src/test/java/ru/job4j/testtask/AnalizeTest.java
@@ -0,0 +1,68 @@
+package ru.job4j.testtask;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class AnalizeTest {
+
+    @Test
+    public void whenCountNewUsers() {
+        Analize analize = new Analize();
+        Analize.User one = new Analize.User(1, "1");
+        Analize.User two = new Analize.User(2, "2");
+        Analize.User three = new Analize.User(3, "3");
+        Analize.User four = new Analize.User(4, "4");
+        Analize.User five = new Analize.User(5, "5");
+        Analize.User six = new Analize.User(5, "5");
+
+        List<Analize.User> previous = new ArrayList<>();
+        previous.add(one);
+        previous.add(two);
+        previous.add(three);
+        List<Analize.User> current = new ArrayList<>(previous);
+
+        current.add(four);
+        current.add(five);
+        current.add(six);
+        previous.get(0).setName("111");
+        int result = analize.newUsers(previous, current);
+        Assert.assertThat(result, is(2));
+        assertThat(Analize.User.edited, is(1));
+    }
+
+    @Test
+    public void whenAddEditDeleteUsers() {
+        Analize analize = new Analize();
+        Analize.User one = new Analize.User(1, "1");
+        Analize.User two = new Analize.User(2, "2");
+        Analize.User three = new Analize.User(3, "3");
+        Analize.User four = new Analize.User(4, "4");
+        Analize.User five = new Analize.User(5, "5");
+        Analize.User six = new Analize.User(5, "5");
+
+        List<Analize.User> previous = new ArrayList<>();
+        previous.add(one);
+        previous.add(two);
+        previous.add(three);
+        List<Analize.User> current = new ArrayList<>(previous);
+
+        current.add(four);
+        current.add(five);
+        current.add(six);
+        previous.get(0).setName("111");
+
+        Analize.Info result = analize.diff(previous, current);
+        assertThat(result.getAdded(), is(2));
+        assertThat(result.getDeleted(), is(0));
+        assertThat(result.getChanged(), is(1));
+    }
+
+
+}
