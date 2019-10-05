e896ec3785eb4375f1782a0c563a76310b37fc27
Mikita Mironenka <m.mironenka@gmail.com>
2019-10-05 21:27:50 +0300

    Читаем файл конфигурации[#181039]

---
+++ chapter_006_IO/src/main/java/ru/job4j/readconfig/Config.java
@@ -0,0 +1,57 @@
+package ru.job4j.readconfig;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+//Примечания. Для работы с потоками ввода-вывода нужно использовать конструкцию try-with-resources.
+//Задание.
+//1. Реализуйте метод load по аналогии с методом toString. Метод load должен загружать пару ключ-значение в Map values.
+//Метод load - должен считать все ключи в карту values.
+// Важно в файле могут быть пустые строки и комментарии их нужно пропускать.
+//2. Реализуйте метод value(String key) он должен возвращать значение ключа.
+//3. Напишите тест ConfigTest.
+
+public class Config {
+    private final String path;
+    private  Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public Map<String, String> getValues() {
+        return this.values;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines()
+                    .filter(line -> !line.contains("##") && !line.isEmpty())
+                    .forEach(line -> this.values.putIfAbsent(line.split("=")[0], line.split("=")[1])
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String value(String key) {
+        return this.values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+}
---
+++ chapter_006_IO/src/test/java/ru/job4j/readconfig/ConfigTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.readconfig;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    @Test
+    public void whenReadPropertiesThenReturnMap() {
+        String path = "src/test/resources";
+        File file = new File(path);
+        String absolutePath = file.getAbsolutePath();
+        Config config = new Config(absolutePath + "/app.properties");
+        config.load();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        map.put("hibernate.connection.url", "jdbc:postgresql://127.0.0.1:5432/trackstudio");
+        map.put("hibernate.connection.driver_class", "org.postgresql.Driver");
+        map.put("hibernate.connection.username", "postgres");
+        map.put("hibernate.connection.password", "password");
+        assertThat(config.getValues(), is(map));
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+    }
+
+}
