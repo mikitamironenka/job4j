baa6baebaa9faa1dddd72c2f145f8e76de88f1f4
Mikita Mironenka <m.mironenka@gmail.com>
2019-11-28 16:05:13 +0300

    deleted test from parser

--- parserrusql/src/test/java/ru/job4j/DBConnectTest.java
+++ parserrusql/src/test/java/ru/job4j/DBConnectTest.java
@@ -1,17 +1,11 @@
 package ru.job4j;
 
-import org.junit.Test;
-import ru.job4j.database.DBConnect;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-
 public class DBConnectTest {
 
-    @Test
-    public void checkConnection() {
-        DBConnect dbConnect = new DBConnect();
-        assertThat(dbConnect.init(), is(true));
-    }
+//    @Test
+//    public void checkConnection() {
+//        DBConnect dbConnect = new DBConnect();
+//        assertThat(dbConnect.init(), is(true));
+//    }
 
 }
