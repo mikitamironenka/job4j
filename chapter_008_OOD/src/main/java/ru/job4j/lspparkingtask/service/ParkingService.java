e0343f52e241279822e84f17d92c93a296ba4321
Mikita Mironenka <m.mironenka@gmail.com>
2020-02-10 00:33:12 +0300

    2. Расширенное хранилище.[#181065] fix ParkingService class.

--- chapter_008_OOD/src/main/java/ru/job4j/lspparkingtask/service/ParkingService.java
+++ chapter_008_OOD/src/main/java/ru/job4j/lspparkingtask/service/ParkingService.java
@@ -7,11 +7,7 @@ package ru.job4j.lspparkingtask.service;
 // либо на N парковочных мест для легковых машин, стоящих рядом.
 // Необходимо разработать сервис для учета парковки машин.
 
-import ru.job4j.lspparkingtask.places.Place;
 import ru.job4j.lspparkingtask.vehicles.Vehicle;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ParkingService {
 
@@ -23,28 +19,15 @@ public class ParkingService {
         this.truckPlaces = truckPlaces;
     }
 
-    private boolean areThereAnyParkingPlaces() {
-        return false;
-    }
-
-    private boolean isItATruck(Vehicle vehicle) {
-        return false;
-    }
-
-    private boolean areThereAnyTruckParkingPlaces() {
-        return false;
-    }
-
-    private List<Place> areThereNCarPlacesForTruck() {
-        List<Place> result = new ArrayList<>();
-        return result;
-    }
-
     /**
      * Park vehicle
      * @return
      */
-    public boolean parkVehicle() {
+    public boolean parkVehicle(Vehicle vehicle) {
+        return false;
+    }
+
+    public boolean canPark(Vehicle vehicle) {
         return false;
     }
 
