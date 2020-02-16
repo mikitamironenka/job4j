e52dbb59978ce978242f38b001fb975afcdc67f3
Mikita Mironenka <m.mironenka@gmail.com>
2020-02-16 19:56:34 +0300

    2. Расширенное хранилище.[#181065] added unit tests

---
+++ chapter_008_OOD/src/test/java/ru/job4j/lspparkingtask/service/ParkingServiceTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.lspparkingtask.service;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.job4j.lspparkingtask.vehicles.Car;
+import ru.job4j.lspparkingtask.vehicles.Truck;
+
+import static org.junit.Assert.*;
+
+public class ParkingServiceTest {
+
+
+
+    @Test
+    public void whenCarCanParkThenShouldReturnTrue() {
+        ParkingService parkingService = new ParkingService(10, 0);
+        Car car = new Car();
+
+        assertTrue(parkingService.canPark(car));
+    }
+
+    @Test
+    public void whenTruckCanParkThenShouldReturnTrue() {
+        ParkingService parkingService = new ParkingService(10, 1);
+        Truck truck = new Truck();
+
+        assertTrue(parkingService.canPark(truck));
+    }
+
+    @Test
+    public void whenTruckCanParkOnCarPlacesThenShouldReturnTrue() {
+        ParkingService parkingService = new ParkingService(10, 0);
+        Truck truck = new Truck();
+
+        assertTrue(parkingService.canPark(truck));
+    }
+
+    @Test
+    public void whenCarParkThenShouldReturnTrue() {
+        ParkingService parkingService = new ParkingService(10, 1);
+        Car car = new Car();
+
+        assertTrue(parkingService.parkVehicle(car));
+    }
+
+    @Test
+    public void whenTruckParkThenShouldReturnTrue() {
+        ParkingService parkingService = new ParkingService(10, 1);
+        Truck truck = new Truck();
+
+        assertTrue(parkingService.parkVehicle(truck));
+    }
+}
