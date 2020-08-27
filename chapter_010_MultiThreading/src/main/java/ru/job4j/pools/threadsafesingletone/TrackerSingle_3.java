f6813fdaef77a0bf987a141ba18c7c7b3f14d888
Mikita Mironenka <m.mironenka@gmail.com>
2020-08-27 14:52:52 +0300

    ThreadSafe Singleton[102739#317205]

---
+++ chapter_010_MultiThreading/src/main/java/ru/job4j/pools/threadsafesingletone/TrackerSingle_3.java
@@ -0,0 +1,29 @@
+package ru.job4j.pools.threadsafesingletone;
+
+//1. Single checked locking.
+//Инициализация и проверка экземпляра происходит внутри критической секции.
+// Этот шаблон деградирует производительность.
+//Использовать этот шаблон не рекомендуется.
+
+public class TrackerSingle_3 {
+
+    private static TrackerSingle_3 INSTANCE;
+
+    private TrackerSingle_3() {
+    }
+
+    public static synchronized TrackerSingle_3 getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new TrackerSingle_3();
+        }
+        return INSTANCE;
+    }
+
+    public Item add(Item model) {
+        return model;
+    }
+
+    public static void main(String[] args) {
+        TrackerSingle_3 tracker = TrackerSingle_3.getInstance();
+    }
+}
---
+++ chapter_010_MultiThreading/src/main/java/ru/job4j/pools/threadsafesingletone/TrackerSingle_5.java
@@ -0,0 +1,31 @@
+package ru.job4j.pools.threadsafesingletone;
+
+//Его работа стабильна и не влияет не производительность системы.
+//Вывод.
+//Если у вас нет необходимости в ленивой загрузки используется шаблоны из первой группы.
+// Например. инициализация кеша или базы данных.
+//Если в приложении есть затратные ресурсы нужно использовать шаблоны с ленивой загрузкой.
+// Здесь можно использовать только один шаблон - это Holder.
+//Другие шаблоны будут отрицательно влиять на производительность системы.
+
+public class TrackerSingle_5 {
+
+    private TrackerSingle_5() {
+    }
+
+    public static TrackerSingle_5 getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    public Item add(Item model) {
+        return model;
+    }
+
+    private static final class Holder {
+        private static final TrackerSingle_5 INSTANCE = new TrackerSingle_5();
+    }
+
+    public static void main(String[] args) {
+        TrackerSingle_5 tracker = TrackerSingle_5.getInstance();
+    }
+}
