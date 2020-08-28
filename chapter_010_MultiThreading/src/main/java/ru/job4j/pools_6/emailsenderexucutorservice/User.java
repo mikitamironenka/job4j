795bd035fd4a86c0c9f3351eb68d059d53a9f9fa
Mikita Mironenka <m.mironenka@gmail.com>
2020-08-28 10:46:46 +0300

    ExecutorService рассылка почты.[63097#317204]

---
+++ chapter_010_MultiThreading/src/main/java/ru/job4j/pools_6/emailsenderexucutorservice/User.java
@@ -0,0 +1,44 @@
+package ru.job4j.pools_6.emailsenderexucutorservice;
+
+import java.util.Objects;
+
+public class User {
+
+    private final String userName;
+    private final String email;
+
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userName, user.userName) &&
+            Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+            "userName='" + userName + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+    }
+}
