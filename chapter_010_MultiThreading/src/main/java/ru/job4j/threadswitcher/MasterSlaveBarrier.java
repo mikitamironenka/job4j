991e9b4f13a981db0cb7df8ca7b4dcb9e8fa910b
Mikita Mironenka <m.mironenka@gmail.com>
2020-09-09 12:20:39 +0300

    Thread switcher[1108#317240]

--- chapter_010_MultiThreading/src/main/java/ru/job4j/threadswitcher/MasterSlaveBarrier.java
+++ chapter_010_MultiThreading/src/main/java/ru/job4j/threadswitcher/MasterSlaveBarrier.java
@@ -2,19 +2,75 @@ package ru.job4j.threadswitcher;
 
 public class MasterSlaveBarrier {
 
-    public void tryMaster() {
+    private final Object monitor = this;
+    private static boolean writeA = true;
 
+    /**
+     * While condition writeA for master thread is false - the thread wait for better times
+     * @throws InterruptedException
+     */
+    public void tryMaster() throws InterruptedException {
+        synchronized (monitor) {
+            while (!writeA) {
+                wait();
+            }
+            writeA = false;
+            System.out.println("Thread A");
+            Thread.sleep(1000);
+            notify();
+        }
     }
 
-    public void trySlave() {
-
+    public void trySlave() throws InterruptedException {
+        synchronized (monitor) {
+            while (writeA) {
+                wait();
+            }
+            writeA = true;
+            System.out.println("Thread B");
+            Thread.sleep(1000);
+            notify();
+        }
     }
 
     public void doneMaster() {
-
+        synchronized (monitor) {
+            while (true) {
+                try {
+                    tryMaster();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public void doneSlave() {
+        synchronized (monitor) {
+            while (true) {
+                try {
+                    trySlave();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+
+        MasterSlaveBarrier masterSlaveBarrier = new MasterSlaveBarrier();
+        Thread master = new Thread(
+            () -> {
+                masterSlaveBarrier.doneMaster();
+            }
+        );
+        Thread slave = new Thread(
+            () -> {
+                masterSlaveBarrier.doneSlave();
+            }
+        );
+        master.start();
+        slave.start();
     }
 }
